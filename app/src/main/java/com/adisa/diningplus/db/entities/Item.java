b849e2ccb715ba388f955a83d86d7abcbef923e4
ErikBoesen <me@erikboesen.com>
2020-09-11 17:12:59 -0600

    Add Item entity

--- app/src/main/java/com/adisa/diningplus/db/entities/Item.java
+++ app/src/main/java/com/adisa/diningplus/db/entities/Item.java
@@ -1,4 +1,36 @@
 package com.adisa.diningplus.db.entities;
 
-public class Item {
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "items")
+public class Item implements Serializable {
+    @PrimaryKey
+    public int id;
+    public String name;
+    public String ingredients;
+
+    public boolean vegetarian;
+    public boolean vegan;
+
+    public boolean alcohol;
+    public boolean nuts;
+    public boolean shellfish;
+    public boolean peanuts;
+    public boolean dairy;
+    public boolean egg;
+    public boolean pork;
+    public boolean seafood;
+    public boolean soy;
+    public boolean wheat;
+    public boolean gluten;
+    public boolean coconut;
+
+    @ColumnInfo(name = "meal_id")
+    public int mealId;
+    @ColumnInfo(name = "course_id")
+    public int courseId;
 }
