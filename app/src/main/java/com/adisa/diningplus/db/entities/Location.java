196f7d46107adfec61695121b223b8cf3e4cf377
ErikBoesen <me@erikboesen.com>
2020-09-11 16:37:09 -0600

    Add Location entity

---
+++ app/src/main/java/com/adisa/diningplus/db/entities/Location.java
@@ -0,0 +1,37 @@
+package com.adisa.diningplus.db.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity
+public class Location implements Serializable {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "type")
+    private String type;
+
+    @ColumnInfo(name = "is_open")
+    private boolean isOpen;
+
+    @ColumnInfo(name = "capacity")
+    private int capacity;
+
+    @ColumnInfo(name = "latitude")
+    private double latitude;
+
+    @ColumnInfo(name = "longitude")
+    private double longitude;
+
+    @ColumnInfo(name = "address")
+    private String address;
+
+    @ColumnInfo(name = "phone")
+    private String phone;
+}
