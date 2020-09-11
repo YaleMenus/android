d5a31f6d113ba5403a49d803a80b4f8093802761
ErikBoesen <me@erikboesen.com>
2020-09-11 17:12:41 -0600

    Add Course entity

--- app/src/main/java/com/adisa/diningplus/db/entities/Course.java
+++ app/src/main/java/com/adisa/diningplus/db/entities/Course.java
@@ -1,4 +1,17 @@
 package com.adisa.diningplus.db.entities;
 
-public class Course {
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "courses")
+public class Course implements Serializable {
+    @PrimaryKey
+    public int id;
+    public String name;
+    public String email;
+    @ColumnInfo(name = "location_id")
+    public int locationId;
 }
