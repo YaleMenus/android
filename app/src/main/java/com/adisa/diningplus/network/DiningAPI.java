b020de0bc9e5ea308c9b242204a8f6929c19c840
ErikBoesen <me@erikboesen.com>
2020-09-13 12:45:02 -0600

    Fix error in getLocations

--- app/src/main/java/com/adisa/diningplus/network/DiningAPI.java
+++ app/src/main/java/com/adisa/diningplus/network/DiningAPI.java
@@ -43,19 +43,23 @@ public class DiningAPI {
 
     public void getLocations() throws IOException, JSONException {
         JSONArray locationsRaw = new JSONArray(getJSON("locations"));
+        db.getDB().locationDao().clear();
         for (int i = 0; i < locationsRaw.length(); i++) {
             JSONObject locationRaw = locationsRaw.getJSONObject(i);
-            Location location = new Location();
-            location.id = locationRaw.getInt("id");
-            location.name = locationRaw.getString("name");
-            location.type = locationRaw.getString("type");
-            location.isOpen = locationRaw.getBoolean("is_open");
-            location.capacity = locationRaw.getInt("capacity");
-            location.latitude = locationRaw.getDouble("latitude");
-            location.longitude = locationRaw.getDouble("longitude");
-            location.address = locationRaw.getString("address");
-            location.phone = locationRaw.getString("phone");
-            db.getDB().locationDao().insert(location);
+            String type = locationRaw.getString("type");
+            if (type.equals("Residential")) { // TODO: remove if API stops providing non-residential locations
+                Location location = new Location();
+                location.id = locationRaw.getInt("id");
+                location.name = locationRaw.getString("name");
+                location.type = type;
+                location.isOpen = locationRaw.optBoolean("is_open", false);
+                location.capacity = locationRaw.optInt("capacity", 0);
+                location.latitude = locationRaw.optDouble("latitude", 0);
+                location.longitude = locationRaw.optDouble("longitude", 0);
+                location.address = locationRaw.optString("address", null);
+                location.phone = locationRaw.optString("phone", null);
+                db.getDB().locationDao().insert(location);
+            }
         }
     }
 
