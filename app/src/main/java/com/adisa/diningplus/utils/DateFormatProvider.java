05d890029fcc13137d466df49cd99ed8aaf2fa07
ErikBoesen <me@erikboesen.com>
2020-09-17 22:42:37 -0600

    Fix date parsing

--- app/src/main/java/com/adisa/diningplus/utils/DateFormatProvider.java
+++ app/src/main/java/com/adisa/diningplus/utils/DateFormatProvider.java
@@ -8,4 +8,5 @@ public class DateFormatProvider {
     public static final SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
     public static final SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss", Locale.US);
     public static final SimpleDateFormat hour = new SimpleDateFormat("h:mm a", Locale.US);
+    public static final SimpleDateFormat hour24 = new SimpleDateFormat("HH:mm", Locale.US);
 }
