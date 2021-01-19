37ccf8aed7b79ff3dde6460fdb12a88a604a9538
ErikBoesen <me@erikboesen.com>
2021-01-18 19:51:19 -0500

    Add date picker dialog

--- app/src/main/java/com/adisa/diningplus/activities/LocationActivity.java
+++ app/src/main/java/com/adisa/diningplus/activities/LocationActivity.java
@@ -1,5 +1,6 @@
 package com.adisa.diningplus.activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -24,6 +25,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 
@@ -41,7 +43,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     API api;
 
     CollapsingToolbarLayout collapsingToolbar;
@@ -197,6 +199,21 @@ public class LocationActivity extends AppCompatActivity {
     }
 
     public void chooseDate(View view) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                this.date.get(Calendar.YEAR),
+                this.date.get(Calendar.MONTH),
+                this.date.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        this.date.set(Calendar.YEAR, year);
+        this.date.set(Calendar.MONTH, month);
+        this.date.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        this.getMeals();
     }
 
     private class MealsTask extends AsyncTask<Void, Void, Void> {
