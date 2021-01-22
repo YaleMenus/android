65f9290f68ce9740eed815dc059a1fb71687aa9c
ErikBoesen <me@erikboesen.com>
2021-01-22 18:47:16 -0500

    Move location code to id

--- app/src/main/java/com/adisa/diningplus/db/entities/Location.java
+++ app/src/main/java/com/adisa/diningplus/db/entities/Location.java
@@ -11,10 +11,9 @@ import java.io.Serializable;
 @Entity(tableName = "locations")
 public class Location implements Serializable {
     @PrimaryKey
-    public int id;
+    public String id;
     public String name;
     public String shortname;
-    public String code;
     public boolean open;
     public int occupancy;
     public double latitude;
@@ -24,10 +23,9 @@ public class Location implements Serializable {
 
     public static Location fromJSON(JSONObject raw) throws JSONException {
         Location location = new Location();
-        location.id = raw.getInt("id");
+        location.id = raw.getString("id");
         location.name = raw.getString("name");
         location.shortname = raw.getString("shortname");
-        location.code = raw.getString("code");
         location.open = raw.optBoolean("open");
         location.occupancy = raw.optInt("occupancy");
         location.latitude = raw.optDouble("latitude");
