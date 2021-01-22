fb9f5f0815b1684e0ddf6c1ca901570eb6760a98
ErikBoesen <me@erikboesen.com>
2021-01-22 17:13:32 -0500

    Move Nutrition instantiation to fromJSON

--- app/src/main/java/com/adisa/diningplus/db/entities/Nutrition.java
+++ app/src/main/java/com/adisa/diningplus/db/entities/Nutrition.java
@@ -4,6 +4,9 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 
 @Entity(tableName = "nutrition")
@@ -72,4 +75,42 @@ public class Nutrition implements Serializable {
     @PrimaryKey
     @ColumnInfo(name = "item_id")
     public int itemId;
+
+    public static Nutrition fromJSON(JSONObject raw) throws JSONException {
+        Nutrition nutrition = new Nutrition();
+        nutrition.portionSize = raw.getString("portion_size");
+        nutrition.calories = raw.getString("calories");
+        nutrition.totalFat = raw.optString("total_fat");
+        nutrition.saturatedFat = raw.optString("saturated_fat");
+        nutrition.transFat = raw.optString("trans_fat");
+        nutrition.cholesterol = raw.optString("cholesterol");
+        nutrition.sodium = raw.optString("sodium");
+        nutrition.totalCarbohydrate = raw.optString("total_carbohydrate");
+        nutrition.dietaryFiber = raw.optString("dietary_fiber");
+        nutrition.totalSugars = raw.optString("total_sugars");
+        nutrition.protein = raw.optString("protein");
+        nutrition.vitaminD = raw.optString("vitamin_d");
+        nutrition.vitaminA = raw.optString("vitamin_a");
+        nutrition.vitaminC = raw.optString("vitamin_c");
+        nutrition.calcium = raw.optString("calcium");
+        nutrition.iron = raw.optString("iron");
+        nutrition.potassium = raw.optString("potassium");
+        nutrition.totalFatPDV = raw.optInt("total_fat_pdv");
+        nutrition.saturatedFatPDV = raw.optInt("saturated_fat_pdv");
+        nutrition.transFatPDV = raw.optInt("trans_fat_pdv");
+        nutrition.cholesterolPDV = raw.optInt("cholesterol_pdv");
+        nutrition.sodiumPDV = raw.optInt("sodium_pdv");
+        nutrition.totalCarbohydratePDV = raw.optInt("total_carbohydrate_pdv");
+        nutrition.dietaryFiberPDV = raw.optInt("dietary_fiber_pdv");
+        nutrition.totalSugarsPDV = raw.optInt("total_sugars_pdv");
+        nutrition.proteinPDV = raw.optInt("protein_pdv");
+        nutrition.vitaminDPDV = raw.optInt("vitamin_d_pdv");
+        nutrition.vitaminAPDV = raw.optInt("vitamin_a_pdv");
+        nutrition.vitaminCPDV = raw.optInt("vitamin_c_pdv");
+        nutrition.calciumPDV = raw.optInt("calcium_pdv");
+        nutrition.ironPDV = raw.optInt("iron_pdv");
+        nutrition.potassiumPDV = raw.optInt("potassium_pdv");
+        nutrition.itemId = raw.getInt("item_id");
+        return nutrition;
+    }
 }
