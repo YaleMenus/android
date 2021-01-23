272cfe85c31070497c0362dec4a36c0489a2c697
ErikBoesen <me@erikboesen.com>
2021-01-23 11:25:45 -0500

    Convert MenuAdapter to Kotlin

---
+++ app/src/main/java/com/adisa/diningplus/adapters/MenuAdapter.kt
@@ -0,0 +1,110 @@
+package com.adisa.diningplus.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.adisa.diningplus.R
+import com.adisa.diningplus.db.entities.Item
+import com.adisa.diningplus.db.entities.Meal
+import com.adisa.diningplus.utils.DateFormatProvider
+import java.text.ParseException
+import java.util.*
+
+class MenuAdapter(private val context: Context, var meals: List<Meal>,
+                  var items: HashMap<String, List<Item>>) : BaseExpandableListAdapter() {
+
+    // TODO: make this a setter
+    fun updateItems(items: HashMap<String, List<Item>>) {
+        this.items = items
+        notifyDataSetChanged()
+    }
+
+    override fun getChild(mealPosition: Int, itemPosition: Int): Any {
+        return items[meals[mealPosition].name]!![itemPosition]
+    }
+
+    override fun getChildId(mealPosition: Int, itemPosition: Int): Long {
+        return itemPosition.toLong()
+    }
+
+    override fun getChildView(mealPosition: Int, itemPosition: Int,
+                              isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        val item = getChild(mealPosition, itemPosition) as Item
+        if (convertView == null) {
+            val layoutInflater = context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.menu_item, null)
+        }
+        val expandedListTextView = convertView!!.findViewById<View>(R.id.expandedListItem) as TextView
+        expandedListTextView.text = item.name.replace('`', '\'')
+        if (item.restricted) {
+            expandedListTextView.setBackgroundColor(context.resources.getColor(R.color.backgroundMarked))
+            expandedListTextView.setTextColor(context.resources.getColor(R.color.colorMarked))
+        } else {
+            expandedListTextView.setBackgroundColor(context.resources.getColor(R.color.transparent))
+            expandedListTextView.setTextColor(context.resources.getColor(android.R.color.primary_text_light))
+        }
+        return convertView
+    }
+
+    override fun getChildrenCount(mealPosition: Int): Int {
+        return items[meals[mealPosition].name]!!.size
+    }
+
+    override fun getGroup(mealPosition: Int): Any {
+        return meals[mealPosition]
+    }
+
+    override fun getGroupCount(): Int {
+        return meals.size
+    }
+
+    override fun getGroupId(mealPosition: Int): Long {
+        return mealPosition.toLong()
+    }
+
+    override fun getGroupView(mealPosition: Int, isExpanded: Boolean,
+                              convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        val meal = getGroup(mealPosition) as Meal
+        if (convertView == null) {
+            val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.meal, null)
+        }
+        val listTitleTextView = convertView!!.findViewById<View>(R.id.listTitle) as TextView
+        listTitleTextView.text = meal.name
+        listTitleTextView.setTextColor(context.resources.getColor(R.color.primary))
+        val mealTimeTextView = convertView.findViewById<View>(R.id.mealTime) as TextView
+        var startTime: Date? = null
+        var endTime: Date? = null
+        // TODO: support 24 and 12 hour time
+        try {
+            startTime = DateFormatProvider.hour24.parse(meal.startTime)
+            endTime = DateFormatProvider.hour24.parse(meal.endTime)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        mealTimeTextView.text = DateFormatProvider.hour.format(startTime) + "–" + DateFormatProvider.hour.format(endTime)
+        val groupIndicator = convertView!!.findViewById<View>(R.id.help_group_indicator) as ImageView
+        if (isExpanded) {
+            groupIndicator.setImageResource(R.drawable.ic_expand_less_black_24dp)
+        } else {
+            groupIndicator.setImageResource(R.drawable.ic_expand_more_black_24dp)
+        }
+        return convertView
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun isChildSelectable(mealPosition: Int, itemPosition: Int): Boolean {
+        return true
+    }
+
+}
