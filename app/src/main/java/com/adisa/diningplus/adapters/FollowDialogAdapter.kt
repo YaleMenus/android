87c269731f78b85799c8614b9ebf3e7b4e891bf2
ErikBoesen <me@erikboesen.com>
2021-01-23 11:09:22 -0500

    COnvert FollowDialogAdapter to Kotlin

---
+++ app/src/main/java/com/adisa/diningplus/adapters/FollowDialogAdapter.kt
@@ -0,0 +1,76 @@
+package com.adisa.diningplus.adapters
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageButton
+import android.widget.TextView
+import com.adisa.diningplus.R
+import java.util.*
+
+class FollowDialogAdapter(private val context: Context, stringSet: MutableSet<String>) : BaseAdapter() {
+    private val stringList = ArrayList<String>()
+    private val preferences: SharedPreferences
+
+    private inner class ViewHolder {
+        var name: TextView? = null
+        var button: ImageButton? = null
+    }
+
+    override fun notifyDataSetChanged() {
+        super.notifyDataSetChanged()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        val inflater = context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val viewHolder: ViewHolder
+        val item = getItem(position)
+        if (convertView == null) {
+            viewHolder = ViewHolder()
+            convertView = inflater.inflate(R.layout.follow_dialog_item, null)
+            viewHolder.name = convertView.findViewById<View>(R.id.followItemName) as TextView
+            viewHolder.button = convertView.findViewById<View>(R.id.followItemRemove) as ImageButton
+            convertView.tag = viewHolder
+        } else {
+            viewHolder = convertView.tag as ViewHolder
+        }
+        viewHolder.name!!.text = item
+        viewHolder.button!!.setOnClickListener {
+            val currentSet = preferences.getStringSet("followedItems", HashSet()) as HashSet<String>?
+            val newSet = HashSet<String>()
+            newSet.addAll(currentSet!!)
+            newSet.remove(item)
+            stringList.remove(item)
+            val editor = preferences.edit()
+            editor.putStringSet("followedItems", newSet)
+            editor.apply()
+            notifyDataSetChanged()
+        }
+        return convertView!!
+    }
+
+    override fun getCount(): Int {
+        return stringList.size
+    }
+
+    override fun getItem(position: Int): String {
+        return stringList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    init {
+        for (string in stringSet) {
+            stringList.add(string)
+        }
+        preferences = PreferenceManager.getDefaultSharedPreferences(context)
+    }
+}
