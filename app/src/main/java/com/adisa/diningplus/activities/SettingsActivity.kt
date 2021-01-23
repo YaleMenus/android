f7cce2223c12dccb0de9e8fe2c0d9cc7a2d938de
ErikBoesen <me@erikboesen.com>
2021-01-22 23:11:30 -0500

    Convert SettingsActivity to kotlin

---
+++ app/src/main/java/com/adisa/diningplus/activities/SettingsActivity.kt
@@ -0,0 +1,56 @@
+package com.adisa.diningplus.activities
+
+import android.os.Bundle
+import android.preference.MultiSelectListPreference
+import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
+import android.view.MenuItem
+import com.adisa.diningplus.R
+import java.util.*
+
+/**
+ * A [PreferenceActivity] that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ *
+ *
+ * See [
+ * Android Design: Settings](http://developer.android.com/design/patterns/settings.html) for design guidelines and the [Settings
+ * API Guide](http://developer.android.com/guide/topics/ui/settings.html) for more information on developing a Settings UI.
+ */
+class SettingsActivity : AppCompatPreferenceActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupActionBar()
+        fragmentManager.beginTransaction().replace(android.R.id.content, MyPreferenceFragment()).commit()
+    }
+
+    class MyPreferenceFragment : PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.preferences_main)
+            val multiSelectListPreference = findPreference("dietary_restrictions") as MultiSelectListPreference
+            val stringSet = PreferenceManager.getDefaultSharedPreferences(activity).getStringSet("dietary_restrictions", HashSet()) as HashSet<String>?
+            multiSelectListPreference.values = stringSet
+        }
+    }
+
+    /**
+     * Set up the [android.app.ActionBar], if the API is available.
+     */
+    private fun setupActionBar() {
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
